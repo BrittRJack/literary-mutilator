c762c9ebcff35252a82225fc7914ee34f4d40a5c
Brittany jack <jackbrittany54@yahoo.com>
2018-11-18 19:12:59 -0700

    Can't get to upload to the server. I can't see if it work

---
+++ javascript-events.js
@@ -0,0 +1,27 @@
+
+/**
+ * JavaScript Events File
+ **/
+
+/**
+ * Changes the text on a click event
+ **/
+function clickEvent() {
+	var changeString="Resistance is good for you";
+	document.getElementById("click").innerHTML = changeString;
+	document.getElementById("click").sytle.color = "green";
+}
+
+/**
+ *  Changes the color of the selected text
+ **/
+function changeTextColor() {
+	document.getElementById("p2").style.color = "blue";
+}
+
+/**
+ * Changes the background color of a paragraph
+ **/
+function changeBackgroundColor() {
+	document.getElementById("p3").style.backgroundColor = "red";
+}
